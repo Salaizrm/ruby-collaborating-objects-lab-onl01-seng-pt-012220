2b93466febff60413810d067bc3fbd7a98ba9f02
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-17 23:58:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,8 +21,12 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.new(name)
+ def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
   end
 
   def self.find(name)
